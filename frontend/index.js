cb5a5a7b48f48b6c20dca56f36c72aa878498b72
Gulbeyaz <glbzbayram@gmail.com>
2023-09-23 00:02:55 +0300

    Nav elemanı eklendi ve linkler tanımlandı

--- frontend/index.js
+++ frontend/index.js
@@ -1,18 +1,35 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom'
-import AppFunctional from './components/AppFunctional'
-import './styles/reset.css'
-import './styles/styles.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
+import AppFunctional from "./components/AppFunctional";
+import "./styles/reset.css";
+import "./styles/styles.css";
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
     <h1>GRID'e hoşgeldiniz</h1>
+    <nav>
+      <NavLink style={{ marginRight: "2rem" }} to="/" exact>
+        functional
+      </NavLink>
+      <NavLink to="/class-based" exact>
+        Class-Based
+      </NavLink>
+    </nav>
     <Routes>
-      <Route path="/" element={<AppFunctional className="functional" />} />
+      <Route
+        path="/"
+        exact
+        element={<AppFunctional className="functional" />}
+      />
+      <Route
+        path="/class-based"
+        exact
+        element={<AppFunctional className="class-based" />}
+      />
     </Routes>
   </BrowserRouter>
-)
+);
